8e2304d72e5d80550e7720b1e2bc3d2a255b0aec
Dlareg <johngeraldcayabyab@gmail.com>
2022-12-01 09:26:03 +0800

    looks like chrome extension is the solution for scraping

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+# import requests
+
 import requests
 
-response = requests.get('https://phisix-api4.appspot.com/stocks/BDO.2021-01-01.json')
-print(response)
+# import requests
+#
+# response = requests.get(
+#     'https://api.investing.com/api/financialdata/historical/102326?start-date=2021-01-01&end-date=2021-12-01&time-frame=Daily&add-missing-rows=false')
+#
+# print(response)
+#
+# # response = requests.get('https://phisix-api4.appspot.com/stocks/BDO.2021-01-01.json')
+# # print(response)
--- stock/stock_poly.py
+++ stock/stock_poly.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import numpy
 from scipy import stats
 
-df = pd.read_csv('../../machine-learning-study/csv/AC-30dayHistory-Nov02.csv')
-
-df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%m-%d')
-
-X = df['Date']
-y = df['Close']
-
-plt.plot(df['Close'])
-
-plt.show()
+# df = pd.read_csv('../../machine-learning-study/csv/AC-30dayHistory-Nov02.csv')
+#
+# df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d %H:%M:%S').dt.strftime('%m-%d')
+#
+# X = df['Date']
+# y = df['Close']
+#
+# plt.plot(df['Close'])
+#
+# plt.show()
