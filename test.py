db68f9122846e097a076284a13259912ac17262e
Dlareg <johngeraldcayabyab@gmail.com>
2022-12-02 13:38:48 +0800

    can now get sector list

--- test.py
+++ test.py
@@ -18,5 +18,10 @@ payload = {
 # cmDetail('13','234')
 # cmpy_id = 13
 # security_id = 140
-companyList = requests.post('https://edge.pse.com.ph/companyDirectory/search.ax')
-print(companyList.text)
+# companyList = requests.post('https://edge.pse.com.ph/companyDirectory/search.ax')
+# print(companyList.text)
+
+
+# sectors list
+sectorList = requests.post('https://edge.pse.com.ph/common/chgSector.ax', json={"idxId": ""}, headers=headers)
+print(sectorList.json())
