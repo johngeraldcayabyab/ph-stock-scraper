b2d3a50bcf12de0805b5d345a35e79b845f94bd0
Dlareg <johngeraldcayabyab@gmail.com>
2022-12-14 08:52:32 +0800

    initialization refactored

--- initialize.py
+++ initialize.py
@@ -3,69 +3,71 @@ import requests
 from bs4 import BeautifulSoup
 from db import test_connection
 
-connection = test_connection()
-cursor = connection.cursor()
-cursor.execute("SHOW DATABASES")
-is_db_exist = False
 
-for database in cursor:
-    if "ph_stock_scraper" in database:
-        is_db_exist = True
-        break
-
-if is_db_exist:
+def initialize_database():
     connection = test_connection()
     cursor = connection.cursor()
-    cursor.execute("DROP DATABASE ph_stock_scraper")
+    cursor.execute("SHOW DATABASES")
+    is_db_exist = False
+    for database in cursor:
+        if "ph_stock_scraper" in database:
+            is_db_exist = True
+            break
+    if is_db_exist:
+        connection = test_connection()
+        cursor = connection.cursor()
+        cursor.execute("DROP DATABASE ph_stock_scraper")
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE ph_stock_scraper")
+    connection.commit()
 
-cursor = connection.cursor()
-cursor.execute("CREATE DATABASE ph_stock_scraper")
-connection = test_connection()
-cursor = connection.cursor()
-cursor.execute(
-    "CREATE TABLE sectors ("
-    " id INT AUTO_INCREMENT PRIMARY KEY,"
-    " cd_id VARCHAR(255),"
-    " cd_name VARCHAR(255),"
-    " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
-    ")")
 
-cursor.execute(
-    "CREATE TABLE companies ("
-    " id INT AUTO_INCREMENT PRIMARY KEY,"
-    " cmpy_id INT,"
-    " security_id INT,"
-    " name VARCHAR(255),"
-    " symbol VARCHAR(255),"
-    " listing_date DATETIME,"
-    " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
-    ")")
-
-cursor.execute(
-    "CREATE TABLE chart_data ("
-    " id INT AUTO_INCREMENT PRIMARY KEY,"
-    " open DECIMAL(13,4),"
-    " close DECIMAL(13,4),"
-    " high DECIMAL(13,4),"
-    " low DECIMAL(13,4),"
-    " volume DECIMAL(13,4),"
-    " chart_date DATETIME,"
-    " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
-    ")")
-
-headers = {
-    'Content-type': 'application/json',
-    'Accept': 'text/plain'
-}
+def create_tables():
+    connection = test_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        "CREATE TABLE sectors ("
+        " id INT AUTO_INCREMENT PRIMARY KEY,"
+        " cd_id VARCHAR(255),"
+        " cd_name VARCHAR(255),"
+        " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
+        ")")
+    cursor.execute(
+        "CREATE TABLE companies ("
+        " id INT AUTO_INCREMENT PRIMARY KEY,"
+        " cmpy_id INT,"
+        " security_id INT,"
+        " name VARCHAR(255),"
+        " symbol VARCHAR(255),"
+        " listing_date DATETIME,"
+        " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
+        ")")
+    cursor.execute(
+        "CREATE TABLE chart_data ("
+        " id INT AUTO_INCREMENT PRIMARY KEY,"
+        " open DECIMAL(13,4),"
+        " close DECIMAL(13,4),"
+        " high DECIMAL(13,4),"
+        " low DECIMAL(13,4),"
+        " volume DECIMAL(13,4),"
+        " chart_date DATETIME,"
+        " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
+        ")")
+    connection.commit()
 
 
 def insert_sectors():
-    sectors = requests.post('https://edge.pse.com.ph/common/chgSector.ax', json={"idxId": ""}, headers=headers)
-
+    connection = test_connection()
+    cursor = connection.cursor()
+    sectors = requests.post('https://edge.pse.com.ph/common/chgSector.ax', json={"idxId": ""}, headers={
+        'Content-type': 'application/json',
+        'Accept': 'text/plain'
+    })
     for sector in sectors.json():
         sql = "INSERT INTO sectors (cd_id, cd_name) VALUES (%s, %s)"
         val = (sector['cdId'], sector['cdNm'])
         cursor.execute(sql, val)
+    connection.commit()
 
 
 def get_total_pages():
@@ -78,15 +80,15 @@ def get_total_pages():
     soup = BeautifulSoup(company_list.text, 'html.parser')
     pages = soup.find_all("div", class_="paging")[0].contents
     total_pages = 0
-
     for page in pages:
         if page.name == 'span':
             total_pages += 1
-
     return total_pages
 
 
 def insert_companies():
+    connection = test_connection()
+    cursor = connection.cursor()
     for i in range(get_total_pages()):
         companies = requests.post('https://edge.pse.com.ph/companyDirectory/search.ax', json={
             'pageNo': i,
@@ -115,14 +117,15 @@ def insert_companies():
                 listing_date = columns[4].contents[0]
                 f = '%b %d, %Y'
                 listing_date = datetime.datetime.strptime(listing_date, f)
-
                 sql = "INSERT INTO companies (name, symbol, cmpy_id, security_id, listing_date) VALUES (%s, %s, %s, %s, %s)"
                 val = (name, symbol, cmpy_id, security_id, listing_date)
                 cursor.execute(sql, val)
+    connection.commit()
 
 
-# insert_sectors()
-# insert_companies()
-connection.commit()
+initialize_database()
+create_tables()
+insert_sectors()
+insert_companies()
 
 print('initialization done')
