592eaff0f9c67690d74282a4eaa144554daba786
= <johngeraldcayabyab@gmail.com>
2022-12-14 10:01:11 +0800

    initialization add chart getter

--- initialize.py
+++ initialize.py
@@ -1,6 +1,9 @@
 import datetime
 import requests
 from bs4 import BeautifulSoup
+from rq import Queue
+from redis import Redis
+from chart_data_scraper import scrap_and_insert_chart_data
 from db import test_connection
 
 
@@ -125,9 +128,29 @@ def insert_companies():
     connection.commit()
 
 
+def get_all_chart_data():
+    redis_conn = Redis('localhost', 6379)
+    q = Queue(connection=redis_conn)
+
+    connection = test_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM companies")
+    companies = cursor.fetchall()
+
+    for company in companies:
+        q.enqueue(
+            scrap_and_insert_chart_data,
+            cmpy_id=company[1],
+            security_id=company[2],
+            listing_date=company[5].strftime("%m-%d-%Y"),
+            company_id=company[0]
+        )
+
+
 initialize_database()
 create_tables()
 insert_sectors()
 insert_companies()
+get_all_chart_data()
 
 print('initialization done')
