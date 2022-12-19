25f5916158354fc07a9a619219f149ab85f6984c
= <johngeraldcayabyab@gmail.com>
2022-12-19 11:22:21 +0800

    created screener updater

--- daily.py
+++ daily.py
@@ -4,7 +4,7 @@ from redis import Redis
 from rq import Queue
 from chart_data_scraper import scrap_and_insert_chart_data, insert_companies
 from db import test_connection
-from stock_calculations import minervini_scanner
+from stock_calculations import minervini_scanner, compute_screener
 
 
 def get_all_chart_data():
@@ -26,9 +26,18 @@ def get_all_chart_data():
         )
 
 
+def compute_all_chart_data():
+    connection = test_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM companies")
+    companies = cursor.fetchall()
+    for company in companies:
+        compute_screener(company_id=company[0])
+
+
 # insert_companies()
-get_all_chart_data()
+# get_all_chart_data()
 # minervini_scanner(159, with_chart=True)
-
-
 # print((date.today() - timedelta(days=1)).strftime("%m-%d-%Y"))
+
+compute_all_chart_data()
