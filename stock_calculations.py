35138789fe21ad6ec9eacb0dd7f78796acac7465
Dlareg <johngeraldcayabyab@gmail.com>
2022-12-19 11:22:21 +0800

    created screener updater

--- stock_calculations.py
+++ stock_calculations.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 from sqlalchemy import create_engine
+from redis import Redis
+from rq import Queue
+
+from db import test_connection
 
 
 def is_close_above_ma(row):
@@ -15,11 +19,10 @@ def is_close_above_ma(row):
     return False
 
 
-
 def rma(x, n, y0):
-    a = (n-1) / n
-    ak = a**np.arange(len(x)-1, -1, -1)
-    return np.r_[np.full(n, np.nan), y0, np.cumsum(ak * x) / ak / n + y0 * a**np.arange(1, len(x)+1)]
+    a = (n - 1) / n
+    ak = a ** np.arange(len(x) - 1, -1, -1)
+    return np.r_[np.full(n, np.nan), y0, np.cumsum(ak * x) / ak / n + y0 * a ** np.arange(1, len(x) + 1)]
 
 
 def minervini_scanner(company_id, with_chart=False):
@@ -55,7 +58,6 @@ def minervini_scanner(company_id, with_chart=False):
     df['avg_loss'] = rma(df.loss[n + 1:].to_numpy(), n, np.nansum(df.loss.to_numpy()[:n + 1]) / n)
     df['rs'] = df.avg_gain / df.avg_loss
     df['rsi_14'] = 100 - (100 / (1 + df.rs))
-
 
     #
     # this should be the code that would draw lines on when all the things is correct below
@@ -121,4 +123,53 @@ def minervini_scanner(company_id, with_chart=False):
         average_days = np.average(total_days)
         mode = stats.mode(total_days, keepdims=False)
         return {'average': average_days, 'mode': mode[0], 'count': mode[1]}
+    return False
+
+
+def update_chart_data(row):
+    sql = "UPDATE chart_data SET rsi_14 = %s, sma_200 = %s, sma_150 = %s, sma_50 = %s WHERE id = %s"
+    val = (row.rsi_14, row.sma_200, row.sma_150, row.sma_50, row.id)
+    connection = test_connection()
+    cursor = connection.cursor()
+    cursor.execute(sql, val)
+    connection.commit()
+
+
+def compute_screener(company_id):
+    redis_conn = Redis('localhost', 6379)
+    q = Queue(connection=redis_conn)
+    db_connection_str = 'mysql+pymysql://root@localhost/ph_stock_scraper'
+    db_connection = create_engine(db_connection_str)
+    sql = 'SELECT * FROM chart_data WHERE company_id = {0} ORDER BY chart_date ASC'.format(company_id)
+    query = pd.read_sql_query(sql=sql, con=db_connection)
+
+    df = query['close'].to_frame()
+    df['id'] = query['id']
+    df['sma_200'] = df['close'].rolling(200).mean()
+    df['sma_150'] = df['close'].rolling(150).mean()
+    df['sma_50'] = df['close'].rolling(50).mean()
+    df['chart_date'] = pd.to_datetime(query['chart_date'])
+    df['volume'] = query['volume']
+    df['average_above'] = False
+    df = df.sort_values(by='chart_date')
+    df = df.set_index(df['chart_date'])
+    df.dropna(inplace=True)
+
+    n = 14
+
+    df['change'] = df['close'].diff()
+    df['gain'] = df.change.mask(df.change < 0, 0.0)
+    df['loss'] = -df.change.mask(df.change > 0, -0.0)
+    df['avg_gain'] = rma(df.gain[n + 1:].to_numpy(), n, np.nansum(df.gain.to_numpy()[:n + 1]) / n)
+    df['avg_loss'] = rma(df.loss[n + 1:].to_numpy(), n, np.nansum(df.loss.to_numpy()[:n + 1]) / n)
+    df['rs'] = df.avg_gain / df.avg_loss
+    df['rsi_14'] = 100 - (100 / (1 + df.rs))
+
+    df = df.reset_index()
+    for row in df.itertuples():
+        q.enqueue(
+            update_chart_data,
+            row
+        )
+
     return False
