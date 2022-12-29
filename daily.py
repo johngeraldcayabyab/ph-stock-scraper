3a297975d2fb95d954a08852739b49fed8970e2f
= <johngeraldcayabyab@gmail.com>
2022-12-29 09:08:29 +0800

    removed unused comments

--- daily.py
+++ daily.py
@@ -51,7 +51,7 @@ def compute_all_chart_data():
 # override_date = '12-15-2022'
 # insert_companies()
 # get_all_chart_data('12-23-2022', yesterday())
-get_all_chart_data('12-27-2022', '12-27-2022')
+get_all_chart_data('12-28-2022', '12-28-2022')
 # minervini_scanner(159, with_chart=True)
 # print((date.today() - timedelta(days=1)).strftime("%m-%d-%Y"))
 
