ced63caa0778759ff0762fecbfc0146056d2cdb7
Dlareg <johngeraldcayabyab@gmail.com>
2023-05-25 19:52:14 +0800

    refactoring initializer

--- initialize.py
+++ initialize.py
@@ -6,73 +6,69 @@ from sectors import Sector
 from chart_data_scraper import scrap_and_insert_chart_data, insert_companies
 from db import test_connection
 
-# def initialize_database():
-#     connection = test_connection(with_db=False)
-#     cursor = connection.cursor()
-#     cursor.execute("SHOW DATABASES")
-#     is_db_exist = False
-#     for database in cursor:
-#         if "ph_stock_scraper" in database:
-#             is_db_exist = True
-#             break
-#     if is_db_exist:
-#         connection = test_connection()
-#         cursor = connection.cursor()
-#         cursor.execute("DROP DATABASE ph_stock_scraper")
-#     cursor = connection.cursor()
-#     cursor.execute("CREATE DATABASE ph_stock_scraper")
-#     connection.commit()
-#
-#
-# def create_tables():
-#     connection = test_connection()
-#     cursor = connection.cursor()
-#     cursor.execute(
-#         "CREATE TABLE sectors ("
-#         " id INT AUTO_INCREMENT PRIMARY KEY,"
-#         " cd_id VARCHAR(255),"
-#         " cd_name VARCHAR(255),"
-#         " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
-#         ")")
-#     cursor.execute(
-#         "CREATE TABLE companies ("
-#         " id INT AUTO_INCREMENT PRIMARY KEY,"
-#         " cmpy_id INT,"
-#         " security_id INT,"
-#         " name VARCHAR(255),"
-#         " symbol VARCHAR(255),"
-#         " listing_date DATETIME,"
-#         " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
-#         ")")
-#     cursor.execute(
-#         "CREATE TABLE chart_data ("
-#         " id INT AUTO_INCREMENT PRIMARY KEY,"
-#         " open DECIMAL(13,4),"
-#         " close DECIMAL(13,4),"
-#         " high DECIMAL(13,4),"
-#         " low DECIMAL(13,4),"
-#         " volume DECIMAL(19,4),"
-#         " sma_50 DECIMAL(13,4) NULL,"
-#         " sma_150 DECIMAL(13,4) NULL,"
-#         " sma_200 DECIMAL(13,4) NULL,"
-#         " rsi_14 DECIMAL(13,4) NULL,"
-#         " chart_date DATETIME,"
-#         " company_id INT,"
-#         " CONSTRAINT fk_companies FOREIGN KEY (company_id) REFERENCES companies(ID),"
-#         " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
-#         ")")
-#     cursor.execute(
-#         "CREATE TABLE average_up_days ("
-#         " id INT AUTO_INCREMENT PRIMARY KEY,"
-#         " days INT,"
-#         " mode INT,"
-#         " count INT,"
-#         " company_id INT,"
-#         # " CONSTRAINT fk_companies FOREIGN KEY (company_id) REFERENCES companies(ID),"
-#         " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
-#         ")")
-#     connection.commit()
+class Initializer:
+    def initialize(self):
+        self.initialize_database()
+        self.initialize_tables()
+        Sector().get_sectors_and_create_or_update()
+        # insert_companies()
+        print('initialization done')
 
+    def initialize_database(self):
+        connection = test_connection(with_db=False)
+        cursor = connection.cursor()
+        cursor.execute("SHOW DATABASES")
+        is_db_exist = False
+        for database in cursor:
+            if "ph_stock_scraper" in database:
+                is_db_exist = True
+                break
+        if is_db_exist:
+            connection = test_connection()
+            cursor = connection.cursor()
+            cursor.execute("DROP DATABASE ph_stock_scraper")
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE ph_stock_scraper")
+        connection.commit()
+
+    def initialize_tables(self):
+        connection = test_connection()
+        cursor = connection.cursor()
+        cursor.execute(
+            "CREATE TABLE sectors ("
+            " id INT AUTO_INCREMENT PRIMARY KEY,"
+            " cd_id VARCHAR(255),"
+            " cd_name VARCHAR(255),"
+            " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
+            ")")
+        cursor.execute(
+            "CREATE TABLE companies ("
+            " id INT AUTO_INCREMENT PRIMARY KEY,"
+            " cmpy_id INT,"
+            " security_id INT,"
+            " name VARCHAR(255),"
+            " symbol VARCHAR(255),"
+            " listing_date DATETIME,"
+            " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
+            ")")
+        cursor.execute(
+            "CREATE TABLE chart_data ("
+            " id INT AUTO_INCREMENT PRIMARY KEY,"
+            " open DECIMAL(13,4),"
+            " close DECIMAL(13,4),"
+            " high DECIMAL(13,4),"
+            " low DECIMAL(13,4),"
+            " volume DECIMAL(19,4),"
+            " sma_50 DECIMAL(13,4) NULL,"
+            " sma_150 DECIMAL(13,4) NULL,"
+            " sma_200 DECIMAL(13,4) NULL,"
+            " rsi_14 DECIMAL(13,4) NULL,"
+            " chart_date DATETIME,"
+            " company_id INT,"
+            " CONSTRAINT fk_companies FOREIGN KEY (company_id) REFERENCES companies(ID),"
+            " created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP"
+            ")")
+        connection.commit()
 
 # def get_all_chart_data():
 #     redis_conn = Redis('localhost', 6379)
@@ -93,11 +89,7 @@ from db import test_connection
 #         )
 
 
-# initialize_database()
-# create_tables()
-sectorClass = Sector()
-sectorClass.get_sectors_and_create_or_update()
+# sectorClass = Sector()
+# sectorClass.get_sectors_and_create_or_update()
 # insert_companies()
 # get_all_chart_data()
-
-print('initialization done')
