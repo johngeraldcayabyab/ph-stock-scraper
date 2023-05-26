73d0162b4ad0f3ca423fe2a02d2960fef8b18003
= <johngeraldcayabyab@gmail.com>
2023-05-26 15:08:37 +0800

    Added undefined sector in seed

--- sectors.py
+++ sectors.py
@@ -3,6 +3,10 @@ from db import test_connection
 
 
 class Sector:
+    def __init__(self):
+        self.connection = test_connection()
+        self.cursor = self.connection.cursor()
+
     def get_sectors(self):
         return requests.post('https://edge.pse.com.ph/common/chgSector.ax', json={"idxId": ""}, headers={
             'Content-type': 'application/json',
@@ -10,18 +14,16 @@ class Sector:
         }).json()
 
     def create_or_update(self, fetched_sectors):
-        connection = test_connection()
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM sectors")
-        current_sectors = cursor.fetchall()
+        current_sectors = self.get_current_sectors()
         val = []
         for fetched_sector in fetched_sectors:
             fetched_sector_id = fetched_sector['cdId']
             if self.is_new_sector(fetched_sector_id, current_sectors):
                 val.append((fetched_sector['cdId'], fetched_sector['cdNm']))
+        val.append(("Undefined", "Undefined"))
         sql = "INSERT INTO sectors (cd_id, cd_name) VALUES (%s, %s)"
-        cursor.executemany(sql, val)
-        connection.commit()
+        self.cursor.executemany(sql, val)
+        self.connection.commit()
 
     def get_sectors_and_create_or_update(self):
         sectors = self.get_sectors()
@@ -33,3 +35,7 @@ class Sector:
             if fetched_sector_id == current_sector_id:
                 return False
         return True
+
+    def get_current_sectors(self):
+        self.cursor.execute("SELECT id, cd_id FROM sectors")
+        return self.cursor.fetchall()
