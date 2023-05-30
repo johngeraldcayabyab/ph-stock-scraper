1cb55e99fbabbfe8e390a7b40d4438fc1f70b7d4
= <johngeraldcayabyab@gmail.com>
2023-05-30 15:25:23 +0800

    added uuid checking and duplicate removals before database insertions for chart data

--- chart_data_scraper.py
+++ chart_data_scraper.py
@@ -1,7 +1,7 @@
 import datetime
 import requests
 from db import test_connection
-from utils import date_today, yesterday
+from utils import date_today
 
 
 def scrap_and_insert_chart_data(company_id, cmpy_id, security_id, start_date=date_today(), end_date=date_today()):
@@ -17,7 +17,7 @@ def scrap_and_insert_chart_data(company_id, cmpy_id, security_id, start_date=dat
         'Accept': 'application/json, text/javascript, */*; q=0.01'
     })
     chart_data = response.json()['chartData']
-    val = []
+    values = []
     for data in chart_data:
         f = '%b %d, %Y %H:%M:%S'
         open_price = data['OPEN']
@@ -26,12 +26,9 @@ def scrap_and_insert_chart_data(company_id, cmpy_id, security_id, start_date=dat
         low = data['LOW']
         volume = data['VALUE']
         chart_date = datetime.datetime.strptime(data['CHART_DATE'], f)
-        val.append((open_price, close, high, low, volume, chart_date, company_id))
-    sql = "INSERT INTO chart_data (open, close, high, low, volume, chart_date, company_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    cursor.executemany(sql, val)
+        uuid = int(company_id + chart_date.timestamp())
+        values.append((uuid, open_price, close, high, low, volume, chart_date, company_id))
+    unique_values = list(set(map(tuple, values)))
+    sql = "INSERT INTO chart_data (uuid, open, close, high, low, volume, chart_date, company_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.executemany(sql, unique_values)
     connection.commit()
-
-
-# 2 GO Group
-print(yesterday())
-scrap_and_insert_chart_data(22, 609, 532, "05-01-2023", yesterday())
