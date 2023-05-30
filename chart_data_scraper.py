34275817fdc6acc2cfa7d27d0a774c2f30d3368b
= <johngeraldcayabyab@gmail.com>
2023-05-30 14:54:20 +0800

    converted single inserts into batch mysql

--- chart_data_scraper.py
+++ chart_data_scraper.py
@@ -1,7 +1,7 @@
 import datetime
 import requests
 from db import test_connection
-from utils import date_today
+from utils import date_today, yesterday
 
 
 def scrap_and_insert_chart_data(company_id, cmpy_id, security_id, start_date=date_today(), end_date=date_today()):
@@ -17,6 +17,7 @@ def scrap_and_insert_chart_data(company_id, cmpy_id, security_id, start_date=dat
         'Accept': 'application/json, text/javascript, */*; q=0.01'
     })
     chart_data = response.json()['chartData']
+    val = []
     for data in chart_data:
         f = '%b %d, %Y %H:%M:%S'
         open_price = data['OPEN']
@@ -25,7 +26,12 @@ def scrap_and_insert_chart_data(company_id, cmpy_id, security_id, start_date=dat
         low = data['LOW']
         volume = data['VALUE']
         chart_date = datetime.datetime.strptime(data['CHART_DATE'], f)
-        sql = "INSERT INTO chart_data (open, close, high, low, volume, chart_date, company_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        val = (open_price, close, high, low, volume, chart_date, company_id)
-        cursor.execute(sql, val)
+        val.append((open_price, close, high, low, volume, chart_date, company_id))
+    sql = "INSERT INTO chart_data (open, close, high, low, volume, chart_date, company_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    cursor.executemany(sql, val)
     connection.commit()
+
+
+# 2 GO Group
+print(yesterday())
+scrap_and_insert_chart_data(22, 609, 532, "05-01-2023", yesterday())
